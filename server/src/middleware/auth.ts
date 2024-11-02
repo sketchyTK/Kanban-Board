79ab80d6788dfa4145d42c3f1ce29d2defe79e0b
sketchyTK <antkinney@gmail.com>
2024-11-02 14:34:50 -0500

    update client side auth

--- server/src/middleware/auth.ts
+++ server/src/middleware/auth.ts
@@ -1,5 +1,5 @@
 import { Request, Response, NextFunction } from 'express';
-import jwt from 'jsonwebtoken';
+import jwt, { Jwt } from 'jsonwebtoken';
 
 interface JwtPayload {
   username: string;
@@ -7,4 +7,21 @@ interface JwtPayload {
 
 export const authenticateToken = (req: Request, res: Response, next: NextFunction) => {
   // TODO: verify the token exists and add the user data to the request object
+  const authorHeader = req.headers.authorization;
+
+  if (authorHeader) {
+    const token = authorHeader.split(' ')[1];
+
+    const secretKey = process.env.JWT_SECRET_KEY || '';
+
+    jwt.verify(token, secretKey, (err, user) => {
+      if (err) {
+        return res.sendStatus(403);
+      }
+    req.user = user as JwtPayload;
+    return next();
+  });
+} else {
+  res.sendStatus(401);
+}
 };
